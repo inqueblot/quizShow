3f616bc73070d52f3033f7e2234cc2e7b846818d
inqueblot <notminebydesign@gmail.com>
2021-01-08 19:17:38 -0600

    improved JS

--- Assets/js/test.js
+++ Assets/js/test.js
@@ -1,59 +1,87 @@
-//start button
-//create button
-// add event listener
-//button starts countdown
-//button presents problem to user
-
-
-//timer
-//create timer
-//broadcast timer to user
-//timer is tied to lose condition
-//present lose card, record for local storage
-
-//problem
-//choose random problem from list
-//display problem to user
-//create toggle from blank to letter
-//create event listener to record keypress
-//win is ties to solving problem
-//solving problem is pressing correct keystrokes
-//if statement?
-//present win card, record for local storage
-
-
-//win and lose streak
-//recorded from timer or problem outcome
-//needs to be recorded to local storage
-//needs to be called when app loads
-
 var startBtn = document.querySelector('#start-button');
 var win = document.querySelector('#win');
 var lose = document.querySelector('#lose');
-var choiceA = document.querySelector('.choiceA');
-var choiceB = document.querySelector('.choiceB');
-var choiceC = document.querySelector('.choiceC');
-var choiceD = document.querySelector('.choiceD');
-//console.log(choiceA.className)
+var timer = document.querySelector('.timer')
+var correctArr = document.querySelectorAll(".choice-correct");
+var wrongArr = document.querySelectorAll(".choice-wrong");
+var timerCount = 10
+var questionNum = 0
+var timerInterval = 0
+var index = 0
+console.log(typeof document.querySelector('#win'))
+
+var questionArr = document.querySelectorAll('section')
 
 
 
-//console.log(startBtn)
-startBtn.addEventListener("click", function (e) {
-    e.preventDefault();
-    //alert("working");
-    var timerCount = 10;
-    var timerId = setInterval(function () {
+{
+    for (let index = 0; index < questionArr.length; index++) {
+        questionArr[index].classList.add("hidden");
+        questionArr[index].classList.remove("visible");
+
+    }
+    //     getWins();
+    //     getLosses();
+}
+
+
+startBtn.addEventListener("click", function () {
+    startTimer();
+    questionArr[0].classList.add("visible");
+
+})
+
+function startTimer() {
+    // Sets timer
+    var timerInterval = setInterval(function () {
         timerCount--;
-        if (timerCount === 0) {
-            ("game over");
-            clearInterval(timerId);
+        timer.textContent = timerCount;
+        if (timerCount <= 0) {
+            // Clears interval
+            clearInterval(timerInterval);
+            gameOver()
+            return timerInterval;
         }
-    })
-})
+    }, 1000);
 
-//event listeners for choices, check if choice is correct
-choiceA.addEventListener("click", function () {
-    //alert("got it")
+};
 
-})
+
+for (let i = 0; i < correctArr.length; i++) {
+    correctArr[i].addEventListener("click", function () {
+        nextQuestion();
+    });
+    console.log(i)
+
+}
+
+for (let i = 0; i < wrongArr.length; i++) {
+    wrongArr[i].addEventListener("click", function () {
+        timerCount--;
+    });
+}
+// document.querySelector(".choice-correct").addEventListener("click", function () {
+//     nextQuestion();
+// });
+
+//document.querySelector(".choice-wrong").addEventListener("click", function () {
+// timerCount -= 10;
+// });
+
+
+
+function nextQuestion() {
+    questionArr[questionNum].classList.add("hidden");
+    questionArr[questionNum].classList.remove("visible");
+    questionNum++;
+    questionArr[questionNum].classList.add("visible");
+    questionArr[questionNum].classList.remove("hidden");
+    console.log(questionNum);
+    if (questionNum === 4) {
+        youWin()
+    }
+}
+
+function youWin() {
+    win = win + 1;
+};
